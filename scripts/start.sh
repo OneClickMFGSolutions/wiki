128f745fdacc3183b5c8f04b64ee90c392dd55e8
OneClickMFGSolutions <55214101+OneClickMFGSolutions@users.noreply.github.com>
2020-09-24 10:40:23 -0700

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,8 +5,9 @@ docker run \
     --restart unless-stopped  \
     -e "DB_TYPE=sqlite"  \
     -e "DB_FILEPATH=/wiki-database/db.sqlite"  \
-    --mount type=bind,source="/mnt/c/Users/it_admin/Documents/wiki/data",target=/wiki-files \
-    --mount type=bind,source="/mnt/c/Users/it_admin/Documents/wiki",target=/wiki-database \
+    -e VIRTUAL_HOST="info" \
+    --mount type=bind,source="/mnt/a/wiki/data",target=/wiki-files \
+    --mount type=bind,source="/mnt/a/wiki",target=/wiki-database \
     -p 3005:3000 \
     -d \
     mackay-mfg/wiki
